0509a896064ffd6894cf87989b6453ea98283749
Nitipon Apaisri <nitipon.apaisri@gmail.com>
2021-12-08 12:42:40 +0100

    Getting started with user wallet interface

--- src/Views/UserWallet.tsx
+++ src/Views/UserWallet.tsx
@@ -1,8 +1,44 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const UserWallet = () => {
+    const navigate = useNavigate();
+    const backToRamenSwap = () => {
+        navigate("/");
+    };
     return (
         <div className="user-wallet-contents">
-            <Link to="/">Back to RamenSwap</Link>
+            <header>
+                <button onClick={backToRamenSwap}>
+                    <p>Back to RamenSwap</p>
+                </button>
+            </header>
+            <div className="contents">
+                <div className="overview">
+                    <div className="title">
+                        <h2>Overview</h2>
+                    </div>
+                    <div className="container">
+                        <div className="display-block tva">
+                            <h4>Total Value Assets</h4>
+                            <hr />
+                            <div className="number">
+                                <h1>$25604</h1>
+                            </div>
+                        </div>
+                        <div className="display-block tha">
+                            <h4>Total Holding Assets</h4>
+                            <hr />
+                            <div className="number">
+                                <h1>4</h1>
+                            </div>
+                        </div>
+                        <div className="display-block tva">
+                            <h4>Coming Soon</h4>
+                            <hr />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
