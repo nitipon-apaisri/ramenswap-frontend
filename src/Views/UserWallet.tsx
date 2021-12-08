97e5306a4d1e9421a4c78b1de45b7b6aaf912d1c
Nitipon Apaisri <nitipon.apaisri@gmail.com>
2021-12-08 16:55:47 +0100

    Able to display all assets in the wallet for user wallet page

--- src/Views/UserWallet.tsx
+++ src/Views/UserWallet.tsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../store/index";
 import { useNavigate } from "react-router-dom";
 const UserWallet = () => {
+    const context = useContext(AppContext);
     const navigate = useNavigate();
     const backToRamenSwap = () => {
         navigate("/");
     };
+
     return (
         <div className="user-wallet-contents">
             <header>
@@ -29,12 +32,51 @@ const UserWallet = () => {
                             <h4>Total Holding Assets</h4>
                             <hr />
                             <div className="number">
-                                <h1>4</h1>
+                                <h1>{context.wallet[0].assets.length}</h1>
                             </div>
                         </div>
                         <div className="display-block tva">
                             <h4>Coming Soon</h4>
                             <hr />
+                        </div>
+                    </div>
+                </div>
+                <div className="tables">
+                    <div className="transactions">
+                        <div className="title">
+                            <h2>Transactions</h2>
+                        </div>
+
+                        <div className="transactions-wrapper">
+                            <h1>Coming Soon</h1>
+                        </div>
+                    </div>
+                    <div className="assets">
+                        <div className="title">
+                            <h2>Assets</h2>
+                        </div>
+                        <div className="table">
+                            <hr />
+                            <div className="table-head">
+                                <h4>Name</h4>
+                                <h4>Holding</h4>
+                            </div>
+                            <hr />
+                            <div className="assets-wrapper">
+                                {context.wallet[0].assets.map((asset: any, index: any) => {
+                                    return (
+                                        <div className="assets-in-wallet" key={`tokenName${index}`}>
+                                            <div className="asset-name">
+                                                <img src={asset.iconUrl} alt="token-img" />
+                                                <p>{asset.name}</p>
+                                            </div>
+                                            <div className="asset-balance">
+                                                <p>{`${asset.balance} ${asset.symbol}`}</p>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
